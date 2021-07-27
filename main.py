4ecaa96aafb4e694f4a8d46d1b81425435e8b4b7
GhaSilva <ghabriel-silva@outlook.com>
2021-07-27 08:50:30 -0300

    Começando projeto

---
+++ main.py
@@ -0,0 +1,17 @@
+url = "bytebank.com/cambio?moedaDestino=dolar&quantidade=100&moedaOrigem=real"
+
+indice_interrogacao = url.find("?")
+url_base = url[:indice_interrogacao]
+
+url_parametros = url[indice_interrogacao+1:]
+print(url_parametros)
+
+parametro_busca = "quantidade"
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+indice_e_comercial = url_parametros.find("&",indice_valor)
+if indice_e_comercial == -1:
+    valor = url_parametros[indice_valor:]
+else:
+    valor = url_parametros[indice_valor:indice_e_comercial]
+print(valor)
