ebdb6a5288fb6baa0f2c0651478dc26e6ece936d
GhaSilva <ghabriel-silva@outlook.com>
2021-07-27 21:40:43 -0300

    Validando URL

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
-url = "bytebank.com/cambio?moedaDestino=dolar&quantidade=100&moedaOrigem=real"
+#url = "bytebank.com/cambio?moedaDestino=dolar&quantidade=100&moedaOrigem=real"
+url = " "
 
+#Sanitização da URL
+url = url.strip()
+
+#Validação da URL
+if url == "":
+    raise ValueError("A Url está vazia")
+
+#separa base e parâmetros
 indice_interrogacao = url.find("?")
 url_base = url[:indice_interrogacao]
-
 url_parametros = url[indice_interrogacao+1:]
 print(url_parametros)
 
+#busca o valor de um parâmetro
 parametro_busca = "quantidade"
 indice_parametro = url_parametros.find(parametro_busca)
 indice_valor = indice_parametro + len(parametro_busca) + 1
 indice_e_comercial = url_parametros.find("&",indice_valor)
+
 if indice_e_comercial == -1:
     valor = url_parametros[indice_valor:]
 else:
